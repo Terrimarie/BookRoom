455959c9363f313378a4de7e3d589f7a38d74c38
Terri Grierson <tmgrierson@googlemail.com>
2020-09-15 11:10:36 +0000

    deleted files and directorys not needed, changed to materialize over bootstrap

--- app.py
+++ app.py
@@ -1,17 +1,15 @@
 import os
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
-
+from bson.objectid import ObjectId
 if os.path.exists("env.py"):
     import env
 
-
 app = Flask(__name__)
 
-
-app.config["DB_NAME"] = "book_room"
+app.config["MONGO_DBNAME"] = "book_room"
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
-app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
+app.config["SECRET_KEY"] = "room"
 
 mongo = PyMongo(app)
 
@@ -19,10 +17,17 @@ mongo = PyMongo(app)
 @app.route("/")
 @app.route("/get_books")
 def get_books():
+    books = list(mongo.db.books.find())
+    return render_template("books.html", books=books)
+
+    
+"""@app.route("/")
+@app.route("/get_books")
+def get_books():
     return render_template(
         "books.html",
-        books=mongo.db.books.find(),
-    )
+        books=mongo.db.books.find()
+    )"""
 
 
 @app.route("/index")
@@ -30,9 +35,9 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/browse")
-def browse():
-    return render_template("browse.html")
+@app.route("/books")
+def books():
+    return render_template("books.html")
 
 
 @app.route("/register")
@@ -47,7 +52,7 @@ def login():
 
 if __name__ == "__main__":
     app.run(
-        host=os.environ.get("IP", "0.0.0.0"),
-        port=int(os.environ.get("PORT", "5000")),
+        host=os.environ.get("IP"),
+        port=int(os.environ.get("PORT")),
         debug=True,
     )
